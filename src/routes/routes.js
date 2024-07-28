2dc634cd9f7b4043af7bf9a03c37d1553d08e573
tamahhamat <newman.tamah@gmail.com>
2024-07-28 23:09:04 +0200

    add route path for easier debugging

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,9 @@
+//C:\Users\newma\school_projects\RafifkiProjectFinal\src\routes\routes.js
+
 const express = require('express');
-const path = require('path');
 const router = express.Router();
 
-router.get('/api/test', (req, res) => {
+router.get('/test', (req, res) => {
   res.json({ message: 'CORS is working!' });
 });
 
