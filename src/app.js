0f0d58136aacfbf109a5be4616baddbf622d57e7
tamahhamat <newman.tamah@gmail.com>
2024-07-28 23:10:46 +0200

    debugging and restructuring

--- src/app.js
+++ src/app.js
@@ -1,29 +1,39 @@
+// C:\Users\newma\school_projects\RafifkiProjectFinal\src\app.js
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
 const cors = require('cors');
-const routes = require('./routes/routes'); 
-
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const routes = require('./routes/routes');
 
-app.use(cors());
-
+app.use(cors({
+    origin: 'http://127.0.0.1:5500',
+    methods: ['GET'],
+}));
 
 app.use(express.json());
 
-
-app.use(express.static(path.join(__dirname, '..', '..', 'Frontend', 'public')));
+const staticPath = path.join(__dirname, '..', '..', 'Frontend', 'public');
+console.log('Serving static files from:', staticPath);
+app.use(express.static(staticPath));
 
 app.use('/', routes);
 
 // Catch-all handler to serve index.html for any other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'Frontend', 'public', 'index.html'));
+    const indexPath = path.join(__dirname, '../Frontend/index.html');
+    console.log('Serving index.html from:', indexPath);
+    res.sendFile(indexPath, (err) => {
+        if (err) {
+            console.error('Error serving index.html:', err);
+            res.status(err.status || 500).end();
+        }
+    });
 });
 
 
