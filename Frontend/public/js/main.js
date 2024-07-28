a8114109eb639a24750211bdc712eeee63845594
tamahhamat <newman.tamah@gmail.com>
2024-07-28 23:09:22 +0200

    moved

--- Frontend/public/js/main.js
+++ Frontend/public/js/main.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('test');
-});
+// C:\Users\newma\school_projects\RafikiProjectFinal\Frontend\public\js\main.js
+
+function App() {
+  return (
+    <div>
+      <h1>Test</h1>
+      <p>This is a React component!</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
